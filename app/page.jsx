63eb539da84568bbcab22c1f9a3314edbbeeda39
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2023-10-24 17:07:37 -0300

    feat: test api

--- app/page.jsx
+++ app/page.jsx
@@ -3,27 +3,41 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { useEffect, useState } from 'react';
 import pokemons from '@/data/poke';
+import axios from 'axios';
 
 export default function Home() {
 
   const empty = '';
 
   const [dataAPI, setDataAPI] = useState(empty);
+  const [allPokemons, setAllPokemons] = useState(empty);
+
+  const getPokemonData = async (url) => {
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
 
   useEffect(() => {
-    const pokeFetch = async () => {
+    const pokeData = async () => {
       try {
         const data = await pokemons();
         setDataAPI(data);
-        console.log('try', dataAPI)
-      } catch (e) {
-        throw e;
+        for (const pokemon of data.data.results) {
+          const data = await getPokemonData(pokemon.url);
+          setAllPokemons(data);
+        }
+      } catch (error) {
+        throw error;
       }
-    };
-    pokeFetch();
+    }
+    pokeData();
   }, []);
 
-  console.log("antesrreturn", dataAPI)
+  console.log(allPokemons)
 
   return (
     <>
