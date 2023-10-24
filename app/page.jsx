2e74fd3fe9ce2f64b79912af8ee6435d4c642f8f
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2023-10-24 13:54:23 -0300

    feat: test API

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,42 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useEffect, useState } from 'react';
+import pokemons from '@/data/poke';
 
 export default function Home() {
+
+  const empty = '';
+
+  const [dataAPI, setDataAPI] = useState(empty);
+
+  useEffect(() => {
+    const pokeFetch = async () => {
+      try {
+        const data = await pokemons();
+        setDataAPI(data);
+      } catch(e) {
+        throw e;
+      }
+    };
+    pokeFetch();
+  }, []);
+
   return (
     <>
+      <div>
+        {
+          dataAPI ? (
+            dataAPI.map((pokemon) => (
+              <div>
+                <p>{pokemon.name}</p>
+              </div>
+            ))
+          ) : (
+            <p>carregando...</p>
+          )
+        }
+      </div>
     </>
   )
 }
