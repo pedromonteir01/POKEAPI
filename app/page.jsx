579ee89b092e2f621945de1e2c4bdf33b97a6362
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2023-10-26 07:57:54 -0300

    feat: api functional

--- app/page.jsx
+++ app/page.jsx
@@ -2,58 +2,85 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import { useEffect, useState } from 'react';
-import pokemons from '@/data/poke';
+import ListaPokemon from '@/models/listapokemon';
 import axios from 'axios';
 
-export default function Home() {
+const pokedex = new ListaPokemon();
 
-  const empty = '';
-
-  const [dataAPI, setDataAPI] = useState(empty);
-  const [allPokemons, setAllPokemons] = useState(empty);
-
-  const getPokemonData = async (url) => {
-    try {
-      const response = await axios.get(url);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  };
+export default function App() {
+  const [allPokemons, setAllPokemons] = useState(pokedex.getAll());
+  const [isLoading, setIsLoading] = useState(true);
+  const [quantity, setQuantity] = useState(32);
 
   useEffect(() => {
-    const pokeData = async () => {
+    async function fetchPokemons() {
       try {
-        const data = await pokemons();
-        setDataAPI(data);
-        for (const pokemon of data.data.results) {
-          const data = await getPokemonData(pokemon.url);
-          setAllPokemons(data);
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${quantity}`);
+        const data = response.data.results;
+
+        const pokemonDetails = [];
+
+        async function fetchPokemonDetails(pokemon) {
+          try {
+            const response = await axios.get(pokemon.url);
+            const pokemonData = {
+              name: response.data.name,
+              sprite: response.data.sprites.front_default,
+              types: response.data.types.map((type) => type.type.name),
+            };
+            pokemonDetails.push(pokemonData);
+          } catch (error) {
+            console.error('Error fetching Pokemon details:', error);
+          }
         }
+
+        // O Promise.all espera que todas as promises sejam resolvidas para continuar.
+        await Promise.all(data.map(fetchPokemonDetails));
+
+        pokedex.fill(pokemonDetails);
+        setAllPokemons(pokedex.getAll());
+        setIsLoading(false);
       } catch (error) {
-        throw error;
+        console.error('Error fetching data:', error);
+        setIsLoading(false);
       }
     }
-    pokeData();
-  }, []);
 
-  console.log(allPokemons)
+    fetchPokemons();
+  }, [quantity]);
+
+  console.log('allPokemons', allPokemons);
 
   return (
-    <>
-      <div>
-        {
-          dataAPI ? (
-            dataAPI.map((pokemon) => (
-              <div key={pokemon.name}>
-                <p>{pokemon.name}</p>
-              </div>
-            ))
-          ) : (
-            <p>carregando...</p>
-          )
-        }
+    <div className={styles.App}>
+      <h1>Pokédex</h1>
+
+      <div className={styles.Quantity}>
+        <label htmlFor="quantity">Quantidade de Pokémons:</label>
+        <input
+          type="number"
+          id="quantity"
+          name="quantity"
+          min="1"
+          max="1000"
+          value={quantity}
+          onChange={(event) => setQuantity(event.target.value)}
+        />
       </div>
-    </>
-  )
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className={styles.PokemonList}>
+          {allPokemons.map((pokemon, index) => (
+            <li key={index} className={styles.PokemonItem}>
+              <h2 className={styles.PokemonName}>{pokemon.name}</h2>
+              <img src={pokemon.sprite} alt={pokemon.name} className={styles.PokemonImage} />
+              <p className={styles.PokemonTypes}>Tipos: {pokemon.types.join(', ')}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
