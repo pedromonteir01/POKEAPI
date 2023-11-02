694512f609b3baee4a2c6f66def118ef32e94da0
ElPedron <pedrohenriquesilva@aluno.senai.br>
2023-11-02 18:54:39 -0300

    feat: delet and edit

--- app/components/card/Card.jsx
+++ app/components/card/Card.jsx
@@ -1,14 +1,29 @@
+'use client'
 import styles from '../../page.module.css';
 
-const Card = ({ name, image, types, index, show }) => {
-    //console.log("Card");
-    //console.log(show);
+const Card = ({ name, image, types, index, show, setRegister, pokemons, modelPokemons, id }) => {
+
+    const delet = (id) => {
+        const pokemon = modelPokemons.getById(id);
+        modelPokemons.deletePokemon(pokemon);
+        console.log(modelPokemons);
+    }
+
+    const edit = () => {
+
+    }
     
     return (
-        <li key={index} className={styles.PokemonItem} onClick={show}>
+        <li key={index} className={styles.PokemonItem} >
+            <div className={styles.btn}>
+                <button onClick={() => delet(id)}>Ex</button>
+                <button>Ed</button>
+            </div>
+            <div onClick={show}>
             <h2 className={styles.PokemonName}>{name}</h2>
             <img src={image} alt={name} className={styles.PokemonImage} />
             <p className={styles.PokemonTypes}>Tipos: {types.join(', ')}</p>
+            </div>
         </li>
     );
 }
