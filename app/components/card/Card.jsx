aac35a39600b681f5f7da01e9f35ef9be19b9829
ElPedron <pedrohenriquesilva@aluno.senai.br>
2023-11-06 23:05:37 -0300

    fix: btn card

--- app/components/card/Card.jsx
+++ app/components/card/Card.jsx
@@ -5,10 +5,6 @@ const Card = ({ name, image, types, index, show, setRegister, pokemons, modelPok
     
     return (
         <li key={index} className={styles.PokemonItem} >
-            <div className={styles.btn}>
-                <button onClick={delet}>Ex</button>
-                <button onClick={edit}>Ed</button>
-            </div>
             <div onClick={show}>
             <h2 className={styles.PokemonName}>{name}</h2>
             <img src={image} alt={name} className={styles.PokemonImage}/>
